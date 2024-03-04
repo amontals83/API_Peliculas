488a8948ea0e0ed18b0a1969b2b2eb044021c6fa
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2024-03-04 14:31:49 +0100

    240204-2

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using API_Peliculas.Modelos.Dtos;
+﻿using API_Peliculas.Modelos;
+using API_Peliculas.Modelos.Dtos;
 using API_Peliculas.Repositorio.IRepositorio;
 using AutoMapper;
 using AutoMapper.Execution;
@@ -38,10 +39,10 @@ namespace API_Peliculas.Controllers
 
         //12º PASO
         [HttpGet("{categoriaId:int}", Name = "GetCategoria")]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)] //EndPoints
-        [ProducesResponseType(StatusCodes.Status200OK)] //EndPoints
-        [ProducesResponseType(StatusCodes.Status400BadRequest)] //EndPoints
-        [ProducesResponseType(StatusCodes.Status404NotFound)] //EndPoints
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetCategoria(int categoriaId)
         {
             var itemCategoria = _ctRepo.GetCategoria(categoriaId);
@@ -51,6 +52,34 @@ namespace API_Peliculas.Controllers
             var itemCategoriaDto = _mapper.Map<CategoriaDto>(categoriaId);
 
             return Ok(itemCategoriaDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(CategoriaDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CrearCategoria([FromBody] CrearCategoriaDto crearCategoriaDto) //LO RECIBE EN FORMATO JSON
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (crearCategoriaDto == null) return BadRequest(ModelState);
+            
+            if (_ctRepo.ExisteCategoria(crearCategoriaDto.Nombre))
+            {
+                ModelState.AddModelError("", "La categoría ya existe");
+                return StatusCode(404, ModelState);
+            }
+
+            var categoria = _mapper.Map<Categoria>(crearCategoriaDto);
+
+            if (!_ctRepo.CrearCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salió mal al guardar el registro{categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetCategoria", new { categoriaId = categoria.Id }, categoria);
         }
     }
 }
--- Modelos/Dtos/CategoriaDto.cs
+++ Modelos/Dtos/CategoriaDto.cs
@@ -8,7 +8,7 @@ namespace API_Peliculas.Modelos.Dtos
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio")]
-        [MaxLength(60, ErrorMessage = "El número máximo de caracteres es de 60")]
+        [MaxLength(100, ErrorMessage = "El número máximo de caracteres es de 60")]
         public string Nombre { get; set; }
     }
 }
